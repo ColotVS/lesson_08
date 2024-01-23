a9388693bd80a1a68cac0e48fe9c81189085d9e5
ColotVS <kolotilov98@mail.ru>
2024-01-23 14:49:55 +0700

    Infinity Урок 8, задача 5

---
+++ lesson_08_05.java
@@ -0,0 +1,13 @@
+public class lesson_08_05 {
+    //В методе main вызови метод div() 2 раза. Первый раз — с такими аргументами,
+    //чтобы вывод в консоли был "Infinity", а второй — "-Infinity".
+    //Код метода div() изменять нельзя.
+    public static void main(String[] args) {
+        div(0.0,100.0);
+        div(0.0,-100.0);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
