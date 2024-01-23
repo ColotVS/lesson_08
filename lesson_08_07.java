68f0b072431cee7bb4f5913a96b85f242348ff8b
ColotVS <kolotilov98@mail.ru>
2024-01-23 16:51:58 +0700

    Работа с типом char Урок 8, задача 7

---
+++ lesson_08_07.java
@@ -0,0 +1,32 @@
+public class lesson_08_07 {
+    //Работа с типом char
+    public static char char1 = 101;
+    public static char char2 = 'H';
+    public static char char3 = 114;
+    public static char char4 = '\u006F';
+    public static char char5 = 108;
+    public static char char6 = 'h';
+    public static char char7 = 0x0063;
+    public static char char8 = ' ';
+    public static char char9 = 'l';
+    public static char char10 = 0x61;
+    public static void main(String[] args) {
+        char a = 'A';
+        a++;
+        System.out.println(a); // На экран будет выведена латинская буква B. т.к её номер 66
+
+        //В методе main() измени порядок вызова методов System.out.print() таким образом, чтобы вывод на консоль был: "Hello char".
+        //Значение переменных не изменяй. В методе main() никакого кода больше не дописывай.
+        System.out.print(char2);
+        System.out.print(char1);
+        System.out.print(char5);
+        System.out.print(char9);
+        System.out.print(char4);
+        System.out.print(char8);
+        System.out.print(char7);
+        System.out.print(char6);
+        System.out.print(char10);
+        System.out.print(char3);
+        System.out.println();
+    }
+}
