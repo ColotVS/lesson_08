62c9d29f84ef2d1e716a383e0ede85a12a29aa3f
ColotVS <kolotilov98@mail.ru>
2024-02-03 13:14:26 +0700

    Наследование, часть 3 Урок 8, задача 24

--- Woman.java
+++
@@ -1,3 +0,0 @@
-public class Woman extends Human{
-
-}
--- lesson_08_24.java
+++ lesson_08_24.java
@@ -2,3 +2,14 @@ public class lesson_08_24 {
     //Построй правильную цепочку наследования классов.
     //Женщина должна наследоваться от человека, а человек от землянина.
 }
+class Woman extends Human{
+
+}
+
+class Human extends Terran{
+
+}
+
+class Terran{
+
+}
