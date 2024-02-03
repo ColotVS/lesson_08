cd4d738a6448e2f626e5c6ba89fd342cc3d7be0c
ColotVS <kolotilov98@mail.ru>
2024-02-03 12:30:01 +0700

    Создание объектов Урок 8, задача 16

---
+++ lesson_08_16.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class lesson_08_16 {
+    //Используя оператор new в методе main(), создай:
+    //
+    //10 объектов типа String;
+    //5 объектов типа int[] (массивов значений int);
+    //2 объекта типа Scanner.
+    public static void main(String[] args) {
+        String a1 = new String("1");
+        String a2 = new String("2");
+        String a3 = new String("3");
+        String a4 = new String("4");
+        String a5 = new String("5");
+        String a6 = new String("6");
+        String a7 = new String("7");
+        String a8 = new String("8");
+        String a9 = new String("9");
+        String a0 = new String("0");
+        int[] b1 = new int[5];
+        int[] b2 = new int[5];
+        int[] b3 = new int[5];
+        int[] b4 = new int[5];
+        int[] b5 = new int[5];
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+    }
+}
